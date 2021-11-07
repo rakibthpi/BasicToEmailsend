a50907463fe56393ce4525982db9abbdee9e99c7
Md.Rakib Hosen <37983022+rakibthpi@users.noreply.github.com>
2021-11-07 20:18:57 +0600

    switch
    switch php

---
+++ index.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Array and Email send</title>
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<link rel="stylesheet" href="assets/css/style.css">
+	<link rel="stylesheet" href="assets/css/responsive.css">
+</head>
+<body>
+	<?php
+
+		// $number1 = 10;
+		// $number2	= 20;
+
+		// $add = $number1 + $number2;
+
+		// echo "This is add number = ".$add."<br>";
+
+		// $start1 = 5;
+		// $start2 = 2;
+
+		// $addition = $start1 ** $start2;
+
+		// echo "Arithmetic Oparator = ".$addition;
+		$number = 12;
+		if($number != 11){
+			echo "Must be 11 number!";
+		}
+		else{
+			echo "success";
+		}
+
+
+
+	?>
+
+</body>
+</html>
